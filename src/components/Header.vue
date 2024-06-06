dc69a7cc1515e61d90e7f4ddd9ba21231dce4cd6
jemshMarcel <jemshjms@gmaill.com>
2024-06-06 17:10:19 +0300

    add component header and data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,23 @@
-<template scoped>
-  <div>
-    
-  </div>
-</template>
-<script setup> 
+<script setup>
+import { navData } from '@/data/data';
+import { ref } from 'vue'
 
-</script>
-<style scoped>
-  
-</style>
+const navs = ref(navData);
+
+</script> 
+
+<template>
+  <header id="home">
+    <RouterLink to="/" class="logo">iChair</RouterLink>
+
+    <ul class="nav">
+      <li v-for="(nav) in navs" :key="nav.id
+      ">
+    <RouterLink to="/">{{ nav.name }}</RouterLink>  
+    </li>
+    </ul>
+    <div class="features"></div>
+    
+  </header>
+</template>
+
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css' ;
 
 import 'bootstrap-icons/font/bootstrap-icons.css' ;
 
+import './assets/main.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
