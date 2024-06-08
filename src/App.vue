ca887611371becd3e610dc068d245decc1aa2995
jemshMarcel <jemshjms@gmaill.com>
2024-06-08 15:39:57 +0300

    Add component sideBar and style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from "./components/Header.vue";
+import SiderBar from "@/components/SideBar.vue";
 
 </script>
 
 <template>
  
     <div>
+      <SiderBar/>
       <Header/>
     </div>
   
---
+++ src/components/SideBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="sideBar">
+    <a href="#" class="menu">
+      <i class="bi bi-menu-app-fill"></i>
+    </a>
+    <ul class="sci" >
+      <li v-for="sci in scis" :key="sci.id">
+        <a href="#">
+          <i :class="sci.icon"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup>
+
+const scis = [
+  {id:1, icon:'bi bi-facebook'},
+  {id:2, icon:'bi bi-twitter-x'},
+  {id:3, icon:'bi bi-instagram'},
+];
+
+</script>
+<style scoped>
+.sideBar{
+  position: fixed;
+  width: 5%;
+  min-height: 100vh;
+  padding: 30px;
+  background: var(--primary);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  transition: 1s;
+  z-index: 2000;
+}
+.menu{
+  font-size: 1.6rem;
+  color: #000;
+  z-index: 2000;
+  transition: ease 1s;
+}
+.menu.active{
+  transform: rotateZ(100deg);
+}
+.sci{
+  padding: 0;
+  display: inline-flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  gap: 25px;
+}
+.sci li {
+  list-style: none;
+}
+.sci li a{
+  font-size: 1.6rem;
+  color: #000;
+}
+
+</style>
