a47e6ae33c9032f8a0283f750eca810d820f8236
jemshMarcel <jemshjms@gmaill.com>
2024-06-08 12:03:49 +0300

    update title

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import 'bootstrap-icons/font/bootstrap-icons.css' ;
 import '@/assets/main.css';
 
 import { createApp } from 'vue'
+
 import App from './App.vue'
+
 import router from './router'
 
 const app = createApp(App)
